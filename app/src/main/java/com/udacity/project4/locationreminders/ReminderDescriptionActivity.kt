d3aa3eca19e66f331662360752b3847d46518803
admin <admin@admins-MacBook-Pro.local>
2021-06-29 12:28:48 +0100

    detail screen displayed on click of notification

--- app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -33,5 +33,8 @@ class ReminderDescriptionActivity : AppCompatActivity() {
             R.layout.activity_reminder_description
         )
 //        TODO: Add the implementation of the reminder details
+
+        //binding.reminderDataItem = intent.getParcelableExtra(EXTRA_ReminderDataItem)
+        binding.reminderDataItem = intent.getSerializableExtra(EXTRA_ReminderDataItem) as ReminderDataItem?
     }
 }
